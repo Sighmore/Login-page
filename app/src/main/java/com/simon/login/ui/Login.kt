48ce954fa0946d54c160a8bf195b6390a909d0ae
INOXOFT <simonkuriah7@gmail.com>
2024-10-08 11:37:49 +0300

    initial commit

---
+++ app/src/main/java/com/simon/login/ui/Login.kt
@@ -0,0 +1,113 @@
+package com.simon.login.ui
+
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.simon.login.R
+
+@Preview
+@Composable
+fun LoginPage(){
+
+    var userName by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    Column(modifier = Modifier
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        
+        Image(
+            painter = painterResource(id = R.drawable.shopify),
+            contentDescription = "Login Image",
+            modifier = Modifier.size(100.dp)
+            )
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(text = "Welcome back", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+        Text(text = "login to your account")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = userName,
+            onValueChange = {
+                userName=it
+                            },
+            label = { Text(text = "Username")}
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password=it},
+            label = { Text(text = "Password")}, visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { Log.i("credentials","Username: $userName Password: $password") }) {
+            Text(text = "Login")
+        }
+        
+        Spacer(modifier = Modifier.height(10.dp))
+
+        TextButton(onClick = { Log.i("Reset password", userName) }) {
+            Text(text = "Forgot password")
+        }
+        
+        Text(text = "Login with")
+
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(30.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Image(modifier = Modifier.clickable { Log.i("Github", "Google login") },
+                painter = painterResource(id = R.drawable.google),
+                contentDescription = "Login with google"
+            )
+            Image(modifier = Modifier.clickable { Log.i("Github", "LinkedIn login") },
+                painter = painterResource(id = R.drawable.linkedln),
+                contentDescription = "Login with linkedIn"
+            )
+            Image(modifier = Modifier.clickable { Log.i("Github", "Git login") },
+                painter = painterResource(id = R.drawable.github),
+                contentDescription = "Login with github"
+            )
+        }
+
+
+    }
+}
